62af113bd962b890aeb4d16097d0c155cf9c917b
benjamin-cognizant <Benjamin.Luke@Cognizant.com>
2026-06-02 06:20:56 +0530

    Reorganized the code to the backend directory

---
+++ backend/src/main/java/com/cognizant/hrms/model/Candidate.java
@@ -0,0 +1,7 @@
+package com.cognizant.hrms.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Candidate {
+}
---
+++ backend/src/test/java/com/cognizant/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
