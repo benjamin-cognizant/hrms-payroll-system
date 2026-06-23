3f69dc912946964485a31623435eebd6c982bdc6
benjamin-cognizant <Benjamin.Luke@Cognizant.com>
2026-06-23 14:30:35 +0530

    Main class created

--- backend/src/main/java/com/hrms/employee/Application.java
+++ backend/src/main/java/com/hrms/employee/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrmsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrmsApplication.class);
+		SpringApplication.run(Application.class);
 	}
 
 }
