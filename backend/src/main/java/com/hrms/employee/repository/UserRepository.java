a71eb620a6b0b4c416285bd04d28b968188244ee
benjamin-cognizant <Benjamin.Luke@Cognizant.com>
2026-06-29 12:08:25 +0530

    Repository class for login

---
+++ backend/src/main/java/com/hrms/employee/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hrms.employee.repository;
+
+import com.hrms.employee.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByLoginnameAndPassword(String loginname, String password);
+}
